b56ef737d05bee2e5209a8cd37cf580fcf222500
MD REYAZ ANSARI <103939454+Mdreyazansari-0786@users.noreply.github.com>
2024-03-29 00:28:49 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-blue-500 text-4xl">hello </h1>
+      <Navbar />
     </div>
   );
 }
