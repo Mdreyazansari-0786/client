93b93a3446220beede190e7694ae931a3688600b
Raushan-kumar88 <raushan.20jics088@jietjodhpur.ac.in>
2024-03-31 22:22:45 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Navbar from "./component/Navbar";
+import Scroll from "./component/Scroll2";
+// import Scroll from "./component/Srolling";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+    <Scroll/>
     </div>
   );
 }
